f81b96b3efbf25cdc52a7ef384c438dfed2eefd9
Reza82 <127042809+Rngpoor@users.noreply.github.com>
2023-03-18 19:10:37 +0330

    Add files via upload

---
+++ Lockkeyboard.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace digikala_project
+{
+    namespace locker
+    {
+        class lockkeyboard
+        {
+            public string y;
+            private string x;
+            private int num;
+            public ConsoleKeyInfo Lock;
+            public int keyboardnumberlocker()
+            {
+                while (true)
+                {
+                    Lock = Console.ReadKey(true);
+                    if (char.IsDigit(Lock.KeyChar))
+                    {
+                        num = num * 10 + (int)char.GetNumericValue(Lock.KeyChar);
+                        Console.Write(Lock.KeyChar);
+                    }
+                    else
+                    {
+                        Console.Beep();
+                    }
+                    if (Lock.Key == ConsoleKey.Enter)
+                    {
+                        Console.WriteLine();
+                        break;
+                    }
+                }
+                return num;
+            }
+
+            public string keyboardYESORNOlocker()
+            {
+                do
+                {
+                    x = Console.ReadKey(true).KeyChar.ToString();
+
+                } while (x.ToLower() != "y" & x.ToLower() != "n");
+                return x;
+            }
+            public string menulocker()
+            {
+                do
+                {
+                    y = Console.ReadKey(true).KeyChar.ToString();
+
+                } while (y.ToLower() != "y" & y.ToLower() != "o" & y.ToLower() != "c" & y.ToLower() != "i" & y.ToLower() != "t");
+                return y;
+            }
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿// Reza Naghipoor
+
+
+
+
+using digikala_project.locker;
+using Digikala1.Model;
+using Digikala1.Operations;
+using Digikala1.Model;
+using Digikala1.Operations;
+
+
+
+
+
+
+
+
+lockkeyboard yn = new lockkeyboard();
+string q;
+do
+{
+    string address = @"C:\digikala\orders.csv";
+
+    DigikalaContext context = new DigikalaContext(address);
+
+    DigikalaOperation op = new DigikalaOperation(context.digikalas);
+
+    lockkeyboard lk = new lockkeyboard();
+
+
+    Console.WriteLine("What do you want? \n " + " order (PRESS O) \n " +
+                      " Customer (PRESS C) \n Item" +
+                      "\n Date (PRESS d) \n City (PRESS s)");
+    string x = lk.menulocker();
+
+
+    switch (x)
+    {
+        case "o" or "O":
+            Console.WriteLine("Enter id-order");
+            int o = lk.keyboardnumberlocker();
+
+            Console.WriteLine(op.priceSalesByorder(o));
+
+            break;
+
+        case "c" or "C":
+            Console.WriteLine("Enter Id-customer");
+            int c = lk.keyboardnumberlocker();
+
+            Console.WriteLine(op.SumSalesBycustomer(c));
+
+            break;
+
+
+        case "i" or "I":
+            Console.WriteLine("Enter id-item");
+            int i = lk.keyboardnumberlocker();
+
+            Console.WriteLine(op.sumSalesByitem(i));
+
+            break;
+
+
+        case "d" or "D":
+            Console.WriteLine("Enter date ");
+            int t = lk.keyboardnumberlocker();
+
+            Console.WriteLine(op.SumSalesByYear(t));
+
+            break;
+
+
+        case "s" or "S":
+            Console.WriteLine("Enter city in persian :");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(op.SumSalescity_name(y));
+
+            break;
+
+        default:
+            throw new Exception("Invalid.............");
+
+    }
+    Console.WriteLine("Continue ? y/n ");
+    q = lk.keyboardYESORNOlocker();
+
+} while (q.ToLower() == "y");
+
+Console.WriteLine("Do you want to search by city? (Y) OR (N)");
+string g = yn.keyboardYESORNOlocker();
+
+
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Thanks to see our site....");
+Console.ResetColor();
